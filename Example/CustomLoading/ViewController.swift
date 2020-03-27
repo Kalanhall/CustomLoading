729ed8480968401065f0465f5984a047985d9be3
Kimee <wujm002@galanz.com>
2020-03-27 16:44:50 +0800

    no message

--- Example/CustomLoading/ViewController.swift
+++ Example/CustomLoading/ViewController.swift
@@ -9,13 +9,23 @@
 import UIKit
 import CustomLoading
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 50
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.description())
+        if #available(iOS 11.0, *) {
+            tableView.contentInsetAdjustmentBehavior = .always
+        } else {
+            self.automaticallyAdjustsScrollViewInsets = true
+        }
         
 //        let header = QQLiveRefreshHeader(frame: CGRect(x: 0,y: 0,width: self.view.bounds.width,height: 50))
 //            self.tableView.handleRefreshHeader(with: header,container:self) { [weak self] in
@@ -39,6 +49,16 @@ class ViewController: UIViewController {
 //        };
 
         self.tableView.switchRefreshHeader(to: .refreshing)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.description())
+
+        return cell!
     }
 
 }
